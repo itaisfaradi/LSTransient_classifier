2cf2a90d0a48c6a5c5c5a30e9bc9befa4978292d
Itai Sfaradi <esfaradi [at] gmail.com>
2026-02-20 10:39:23 -0800

    clean up and documentation

--- running_LSTM_classifier.py
+++ running_LSTM_classifier.py
@@ -10,44 +10,54 @@ from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, St
 import random
 import os
 
-list_of_dicts = pkl.load(open("all_objects_dicts_prog1.pkl", "rb")) # Load preprocessed dataset
-list_of_dicts = [obj for obj in list_of_dicts if "label" in obj] # Filter out objects without labels
-labels = np.array([obj["label"] for obj in list_of_dicts], dtype=np.int64) # Extract labels
-n_pos = int((labels == 1).sum()) # Count positive samples (TDEs)
-n_neg = int((labels == 0).sum()) # Count negative samples (Ia SNe)
-
-pos_weight_value = (n_neg / max(n_pos, 1)) # Compute positive class weight to handle class imbalance
-print("n_pos (TDE):", n_pos, "n_neg (Ia):", n_neg, "pos_weight:", pos_weight_value) # Print class distribution info
-
-pos_weight = torch.tensor([pos_weight_value], dtype=torch.float32) # Create tensor for positive class weight
-
-
+#------------------------------------------------------------------------#
+# Setting random seed #
+#------------------------------------------------------------------------#
 seed = 0
 random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 
-dataset = TwoBandLC(list_of_dicts, T=300.0, L=300, use_err=True, normalize_per_object=True, up_to_peak=False) # Create dataset instance
+#------------------------------------------------------------------------#
+# Setting random seed #
+#------------------------------------------------------------------------#
+device = "cuda" if torch.cuda.is_available() else "mps"
 
-K = 5
+#------------------------------------------------------------------------#
+# Load preprocessed dataset and label #
+#------------------------------------------------------------------------#
+list_of_dicts = pkl.load(open("all_objects_dicts_prog1.pkl", "rb"))
+list_of_dicts = [obj for obj in list_of_dicts if "label" in obj]
+labels = np.array([obj["label"] for obj in list_of_dicts], dtype=np.float32)
+n_pos = int((labels == 1).sum())
+n_neg = int((labels == 0).sum())
+# Compute positive class weight to handle class imbalance
+pos_weight_value = (n_neg / max(n_pos, 1))
+print("n_pos (TDE):", n_pos, "n_neg (Ia):", n_neg, "pos_weight:", pos_weight_value)
+pos_weight = torch.tensor([pos_weight_value], dtype=torch.float32)
+# Create dataset instance
+dataset = TwoBandLC(list_of_dicts, T=300.0, L=300, use_err=True, normalize_per_object=True, up_to_peak=False)
+
+#------------------------------------------------------------------------#
+# Training #
+#------------------------------------------------------------------------#
+K = 5 # number of folds
 batch_size = 128
 n_epochs = 500
 lr = 1e-4
-device = "cuda" if torch.cuda.is_available() else "mps"
 skf = StratifiedKFold(n_splits=K, shuffle=True, random_state=seed)
-y_all = np.array([obj["label"] for obj in list_of_dicts], dtype=np.float32)
-fold_bal_acc = []
+fold_bal_acc = [] # output balanced accuracy
 
-for fold, (train_idx, val_idx) in enumerate(skf.split(np.zeros(len(y_all)), y_all)):
+# iterating on the training fro different K folds
+for fold, (train_idx, val_idx) in enumerate(skf.split(np.zeros(len(labels)), labels)):
     print(f"\n--- Fold {fold+1}/{K} ---")
-    print(f"  Train positives: {y_all[train_idx].sum():.0f}  Val positives: {y_all[val_idx].sum():.0f}")
-    
+    print(f"  Train positives: {labels[train_idx].sum():.0f}  Val positives: {labels[val_idx].sum():.0f}")
     train_ds = Subset(dataset, train_idx)
     val_ds   = Subset(dataset, val_idx)
     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True,  drop_last=False)
     val_loader   = DataLoader(val_ds,   batch_size=batch_size*2, shuffle=False, drop_last=False)
-    model = LSTMClassifier(LSTMConfig())  # fresh model each fold
+    model = LSTMClassifier(LSTMConfig())
     model, history = train(
         model, train_loader, val_loader, device,
         pos_weight=pos_weight, epochs=n_epochs, lr=lr, weight_decay=lr/10,
